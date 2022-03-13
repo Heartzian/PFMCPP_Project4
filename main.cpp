673df9f7cba29ab892d7ff93e1a81855e4395b46
Heartzian <97336494+Heartzian@users.noreply.github.com>
2022-03-13 10:17:05 +0000

    part3() added again

--- main.cpp
+++ main.cpp
@@ -452,6 +452,23 @@ private:
     float x{0}, y{0};
 };
 
+void part3()
+{
+    FloatType ft( 5.5f );
+    DoubleType dt( 11.1 );
+    IntType it ( 34 );
+    DoubleType pi( 3.14 );
+
+    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( ft ).multiply( ft ).divide( it ) << std::endl;
+    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( 3 ).add( it ) << std::endl;
+    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide( static_cast<int>(pi) ).multiply( static_cast<int>(dt) ).subtract( static_cast<int>(ft) ) << std::endl;
+    std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
+    std::cout << it.multiply(it).divide(0).divide(0.0f).divide(0.0) << std::endl;
+    
+    std::cout << "FloatType x IntType  =  " << it.multiply( static_cast<int>(ft) ) << std::endl;
+    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( static_cast<int>(dt) ).add( static_cast<int>(ft) ).multiply( 24 ) << std::endl;
+}
+
 void part4()
 {
     // ------------------------------------------------------------
@@ -588,6 +605,7 @@ int main()
 
     std::cout << "---------------------\n"<< std::endl; 
 
+    part3();
     part4();
     
     std::cout << "good to go!" << std::endl;
